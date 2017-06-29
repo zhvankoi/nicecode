62f91dd7a6a3b19f14dc57ab13960891b6cffb80
Igor Zhvanko <Igor.Zhvanko@powerschool.com>
2017-06-29 17:31:58 +0300

    added raw mode switch and error handling

--- nicecode.c
+++ nicecode.c
@@ -1,6 +1,59 @@
+#include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
-int main() {
-  printf("Hello world!\n");
+struct termios orig_termios;
+
+void handleErrorAndQuit(const char *msg)
+{
+  perror(msg);
+  exit(1);
+}
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    handleErrorAndQuit("disableRawMode: tcsetattr failed");
+}
+
+void enableRawMode()
+{
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    handleErrorAndQuit("enableRawMode: tcgetattr failed");
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    handleErrorAndQuit("enableRawMode: tcsetattr");
+}
+
+int main()
+{
+  enableRawMode();
+
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) 
+      handleErrorAndQuit("main: read failed");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+  }
+
+
   return 0;
 }
